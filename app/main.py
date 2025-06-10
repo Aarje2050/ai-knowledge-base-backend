1c0d8ce51b243ff7fc1085e4e31380cfe31c0fa9
Rajesh <aarje2050@gmail.com>
2025-06-10 18:01:21 +0530

    CORS

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="AI Knowledge Platform", version="1.0.0")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://frontend-ai-knowledge-base.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
